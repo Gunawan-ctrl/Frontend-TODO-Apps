054ff3bb6ea1e535458120c0ee17cc6208a143ce
gunawan <gunawa414@gmail.com>
2023-05-28 09:08:27 +0700

    first commit

---
+++ src/pages/HelpPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">help</h5>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
+      distinctio est fugiat, iure accusantium error autem, voluptates sequi,
+      aspernatur dolore repellendus aut. Est exercitationem quam ad ipsa
+      molestias unde quibusdam.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati beatae
+      enim placeat necessitatibus error adipisci magni dicta, libero distinctio!
+      Veritatis impedit tenetur, molestias soluta commodi voluptates? Et totam
+      vel suscipit.
+    </p>
+  </q-page>
+</template>
---
+++ src/router/routes.js
@@ -0,0 +1,23 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", name: "todo", component: () => import("pages/TodoPage.vue") },
+      {
+        path: "/help",
+        name: "help",
+        component: () => import("pages/HelpPage.vue"),
+      },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
